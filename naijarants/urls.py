6a657c1550fd6d587eb030c50099ec56d51eae4e
dolastack <dejiolawoye@outlook.com>
2017-11-24 18:35:01 -0500

    Changes

--- naijarants/urls.py
+++ naijarants/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'news/', include('newsfeed.urls')),
+    #url(r'news/', include('newsfeed.urls')),
     url(r'', include('rants.urls')),
 
 ]
--- rants/models.py
+++ rants/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Rant(models.Model):
+    title = models.CharField(max_length=100)
+    body = models.TextField()
+    created = models.DateTimeField()
+    
