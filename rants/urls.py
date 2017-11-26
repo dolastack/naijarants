cf8f9eadf3e480acc726623ac8fc9cc35ec327bd
dolastack <dejiolawoye@outlook.com>
2017-11-26 17:14:27 -0500

    test

--- rants/urls.py
+++ rants/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^(rant)/', views.rant_detail, name='rant_detail')
+    url(r'([^/]*)', views.rant_detail, name='rant_detail')
 ]
