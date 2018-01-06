ef3c81d41643bcf232eb094cc8766f9360e82a88
dolastack <dejiolawoye@outlook.com>
2018-01-06 00:03:50 -0500

    upload files

--- rants/views.py
+++ rants/views.py
@@ -34,7 +34,7 @@ def new_rant(request):
         rant = Rant()
         if request.user.is_authenticated:
             rant.author=request.user
-        form = RantForm(instance=rant, data=request.POST)
+        form = RantForm(instance=rant, data=request.POST, request.FILES)
         if form.is_valid():
             form.save()
             return redirect('accounts_user_home')
