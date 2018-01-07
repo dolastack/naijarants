27f9516ae53b14b51948f52de9bf6dd149f3301f
dolastack <dejiolawoye@outlook.com>
2018-01-07 14:37:10 -0500

    fix import

--- rants/views.py
+++ rants/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.views.decorators.cache import cache_page
+from django.conf import settings
 from .models import Rant
 from .forms import RantForm, CommentForm
 
