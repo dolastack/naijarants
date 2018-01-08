d11e1c61b924f34824c0ffe5bacbac0c621b778f
dolastack <dejiolawoye@outlook.com>
2018-01-07 22:42:33 -0500

    test

--- naijarants/settings.py
+++ naijarants/settings.py
@@ -132,15 +132,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
+STATICFILES_DIRS = [
+        os.path.join(BASE_DIR, 'static')
+    ]
 STATIC_URL = '/static/'
 
 #MEDIA_URL = ''
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
-STATICFILES_DIRS = [
-        os.path.join(BASE_DIR, 'static')
-    ]
+
 from django.urls import reverse
 
 LOGIN_REDIRECT_URL = 'accounts_user_home'
