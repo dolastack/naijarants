920489238ce327ff5ebbe084b4c7835c0b529c62
dolastack <dola@localhost.localdomain>
2018-02-01 20:08:40 -0500

    FF

--- naijarants/urls.py
+++ naijarants/urls.py
@@ -30,6 +30,5 @@ urlpatterns = [
 if settings.DEBUG:
     urlpatterns += [
         url(r'^media/(?P<path>.*)$',
-            serve, {'document_root':
-                    settings.MEDIA_ROOT,}),
+            serve, {'document_root': settings.MEDIA_URL,}),
     ]
