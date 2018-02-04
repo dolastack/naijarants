64a4a7837c39c2eb28b9f3ded0455248ef89d966
dolastack <dejiolawoye@outlook.com>
2018-02-04 16:41:06 -0500

    test

--- rants/urls.py
+++ rants/urls.py
@@ -10,9 +10,10 @@ urlpatterns = [
     url(r'(?P<id>\d+)/(?P<rant_title>[^/]*)/$', rant_detail, name='rant_detail'),
 
 ]
-
+"""
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL,
                           document_root=settings.MEDIA_ROOT )
     urlpatterns += static(settings.STATIC_URL,
                           document_root=settings.STATIC_ROOT)
+"""
