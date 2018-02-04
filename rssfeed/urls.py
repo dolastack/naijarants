afbdc094f27f96597a3c47ddd3e9ce0070dd484a
dolastack <dejiolawoye@outlook.com>
2018-02-04 16:48:36 -0500

    test

--- rssfeed/urls.py
+++ rssfeed/urls.py
@@ -1,5 +1,16 @@
 from django.conf.urls import url
 from .views import articles_list
+from django.conf import settings
+from django.views.static import serve
+from django.conf.urls.static import static
+
+
 urlpatterns = [
     url('', articles_list, name='index' )
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
+
+    urlpatterns += static(settings.STATIC_URL,
+                          document_root=settings.STATIC_ROOT)
