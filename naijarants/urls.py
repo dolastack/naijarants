5f626e7ac037e12de8f1470baf8a5fea3aa628c0
dolastack <dejiolawoye@outlook.com>
2018-02-06 17:20:16 -0500

    t

--- naijarants/urls.py
+++ naijarants/urls.py
@@ -26,6 +26,6 @@ urlpatterns = [
     url(r'^about$', about, name='about_page'),
     url(r'^rant/', include('rants.urls')),
     url(r'^account/', include('accounts.urls')),
-    url(r'(?P<category>[^/]*)', rants_by_category , name='rants_by_catogory'),
+    #url(r'(?P<category>[^/]*)', rants_by_category , name='rants_by_catogory'),
     url(r'^', include('rssfeed.urls')),
 ]
