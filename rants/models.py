12ce46cd596ec435fd4011039500e07db08fcde9
dolastack <dejiolawoye@outlook.com>
2018-02-11 23:31:28 -0500

    d

--- rants/models.py
+++ rants/models.py
@@ -37,7 +37,7 @@ class Rant(models.Model):
         return reverse_lazy('rant_detail', args=[self.id, self.title])
     
     def get_social_url(self):
-        return "http://naijarants.com/rant/{id}/{title}".format( id=self.id + title=self.title )
+        return "http://naijarants.com/rant/{id}/{title}".format( id=self.id , title=self.title )
 
     def __str__(self):
         return self.title
