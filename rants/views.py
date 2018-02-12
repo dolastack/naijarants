880a391585306f43e1bac5548ea1ccbb9fc15b22
dolastack <dejiolawoye@outlook.com>
2018-02-11 23:02:56 -0500

    fr

--- rants/views.py
+++ rants/views.py
@@ -21,7 +21,7 @@ def rants_list(request):
     rants = Rant.objects.all()
     return rants
 
-
+"""
 def rant_detail(request, rant):
    # rant = get_object_or_404(Rant, title=rant_title, pk=id)
 
@@ -55,7 +55,7 @@ def rant_detail(request, id,rant_title):
     context = {'rant': rant, 'comments': comments, 'comment_form': comment_form}
     template = "rants/rant_detail.html"
     return render(request, template, context)
-"""
+
 def new_rant(request):
     if request.method == "POST":
         form = RantForm( request.POST, request.FILES)
