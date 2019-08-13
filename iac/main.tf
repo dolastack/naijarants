4392792341133b997f8af2edf9bbfd6af55bfdb9
Deji Olawoye <dolastack@gmail.com>
2019-08-12 21:54:44 -0400

    update ansible

--- iac/main.tf
+++ iac/main.tf
@@ -7,6 +7,11 @@ provider "aws"{
 resource "aws_instance" "naijarant"{
 	ami="ami-06f2f779464715dc5"
 	key_name="aws-key"
-        instance_type="t2.micro"
+    instance_type="t2.micro"
+
+	associate_public_ip_address = true
+	provisioner "local-exec"  {	
+		command = "sleep 120; ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -u ubuntu --private-key ../aws-key.pem -i '${aws_instance.naijarant.public_ip},' ../setup.yml"
+	}
 }
 
